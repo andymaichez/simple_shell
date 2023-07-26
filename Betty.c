92a900bcf350558a3749f120986c7db00e63c569
andymaichez <andymaichez@gmail.com>
2023-07-26 07:51:23 -0700

    simple shell

--- Betty.c
+++ Betty.c
@@ -6,10 +6,19 @@
  * add_numbers - function to add numbers.
  * @numx: The first number to be added.
  * @numy: The second number to be added.
- * Return: The sum of @num1 and @num2.
+ * Return: The sum of @numx and @numy.
  */
 int add_numbers(int numx, int numy)
 {
 return (numx + numy);
+}
+int main(void)
+{
+int result = add_numbers(5, 12);
+char result_str[30];
+int length = snprintf(result_str,
+	sizeof(result_str), "The result is: %d\n", result);
+write(1, result_str, length);
+return (0);
 }
 
